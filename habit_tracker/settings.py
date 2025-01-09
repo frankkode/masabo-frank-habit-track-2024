6895d59d12078a74dd4195cd46f4fe28273d1b3b
masacode <masasimpafra@gmail.com>
2025-01-09 17:08:53 +0200

    add domain

--- habit_tracker/settings.py
+++ habit_tracker/settings.py
@@ -18,6 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
+#always change to add your domain this is for test purposes
 ALLOWED_HOSTS = ['*']
 
 # Application definition
